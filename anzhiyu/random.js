d4997a1ecc511e38bde6648b631e4ba9bf980ac8
fxck1024 <1950816185@qq.com>
2024-02-01 14:35:28 +0800

    Site updated: 2024-02-01 14:35:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/8544442c.html","posts/490e9480.html","posts/1d5ef26f.html","posts/f255ffad.html"];function toRandomPost(){
+var posts=["posts/c16c6dcf.html","posts/490e9480.html","posts/f255ffad.html","posts/8544442c.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
