9362f40aacad179d2fc515251998a3075ceabbea
fxck1024 <1950816185@qq.com>
2024-07-12 20:59:00 +0800

    Site updated: 2024-07-12 20:58:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/8544442c.html","posts/7fc8ee4.html","posts/f255ffad.html","posts/490e9480.html","posts/c16c6dcf.html"];function toRandomPost(){
+var posts=["posts/7fc8ee4.html","posts/f255ffad.html","posts/490e9480.html","posts/8544442c.html","posts/c16c6dcf.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
