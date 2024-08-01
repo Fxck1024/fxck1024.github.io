e35d833f082d0bd0f9e2e8c9148932fb90d0e90c
fxck1024 <1950816185@qq.com>
2024-08-01 20:06:30 +0800

    Site updated: 2024-08-01 20:06:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/c16c6dcf.html","posts/f255ffad.html","posts/7fc8ee4.html","posts/490e9480.html","posts/8544442c.html"];function toRandomPost(){
+var posts=["posts/c16c6dcf.html","posts/f255ffad.html","posts/7fc8ee4.html","posts/490e9480.html","posts/8544442c.html","posts/f4d746ea.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
