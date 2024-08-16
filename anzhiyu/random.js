32630005bf44eb373079a2d6528b6db8db3eadd1
fxck1024 <1950816185@qq.com>
2024-08-16 15:09:22 +0800

    Site updated: 2024-08-16 15:09:19

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["posts/c16c6dcf.html","posts/f255ffad.html","posts/7fc8ee4.html","posts/490e9480.html","posts/8544442c.html","posts/f4d746ea.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
